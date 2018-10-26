4fe602c5e2c55279cd4da16addba4203a2526440
Raghav Sairam <sairam.r@husky.neu.edu>
2018-10-25 22:13:49 -0400

    backchannel for most recent data files

--- d2api/src/entities.py
+++ d2api/src/entities.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from pathlib import Path
+import os
 import json
+import requests
 
-def load_json(file_name):
+def ensure_data_folder():
+    directory = Path(os.path.join(os.path.dirname(__file__), '..', 'data'))
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def load_local_json(file_name):
     try:
-        f = open(Path('../data/{}'.format(file_name)), 'r')
+        p = Path(os.path.join(os.path.dirname(__file__), '..', 'data', file_name))
+        f = open(p, 'r')
         return json.load(f)
     except IOError:
         return {}
+
+def load_remote_json(file_name):
+    url = "https://raw.githubusercontent.com/whoophee/d2api/d2api/data/{}".format(file_name)
+    res = requests.get(url)
+    if res.status_code == 200:
+        return res.json()
+    else:
+        return {}
+
+def write_local_json(data, file_name):
+    p = Path(os.path.join(os.path.dirname(__file__), '..', 'data', file_name))
+    with open(p, 'w') as outfile:
+        json.dump(data, outfile)
+
 
 def get_side(player_slot):
     return "radiant" if player_slot < 128 else "dire"
@@ -74,7 +96,27 @@ class SteamAccount:
                 self.id64 = account_id
 
 
-all_heroes = load_json('heroes.json')
-all_items = load_json('items.json')
-all_abilities = load_json('abilities.json')
+# Find version of local data
+local_meta = load_local_json('meta.json')
+local_version = local_meta.get('version')
 
+# find version of remote data
+remote_meta = load_remote_json('meta.json')
+remote_version = remote_meta.get('version')
+
+content_files = ['heroes.json', 'abilities.json', 'items.json']
+
+print(local_version, remote_version)
+# update local files if they're outdated
+if local_version != remote_version:
+    ensure_data_folder()
+    write_local_json(remote_meta, "meta.json")
+
+    for content_name in content_files:
+        remote_content = load_remote_json(content_name)
+        write_local_json(remote_content, content_name)
+
+
+all_heroes = load_local_json('heroes.json')
+all_items = load_local_json('items.json')
+all_abilities = load_local_json('abilities.json')
