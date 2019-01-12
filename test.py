0a5a548045c1aff668b1f8b1afbda9bcf63d0d83
Raghav Sairam <raghavsairamn@gmail.com>
2019-01-12 00:45:53 -0500

    additional tests

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import d2api
-from d2api.src.errors import *
-from d2api.src.wrappers import *
-from d2api.src.entities import *
-from d2api.src import endpoints
-import unittest
 import os
+import unittest
+
+import d2api
+from d2api.src import endpoints
+from d2api.src import entities
+from d2api.src import errors as d2errors
+from d2api.src import wrappers
+from d2api import update_local_data
+
 
 class APIPreliminaryTests(unittest.TestCase):
     def test_environment_api_key_set(self):
@@ -17,12 +20,12 @@ class APIPreliminaryTests(unittest.TestCase):
         key = 'abcdxyz'
         tmp_api = d2api.APIWrapper(key)
 
-        with self.assertRaises(APIAuthenticationError, 
+        with self.assertRaises(d2errors.APIAuthenticationError, 
         msg = 'Request with API key \'{}\' should raise authentication error'.format(key)):
             tmp_api.get_match_details('4176987886')
     
     def test_insufficient_params(self):
-        with self.assertRaises(APIInsufficientArguments, msg = "match_id is a required argument for get_match_details"):
+        with self.assertRaises(d2errors.APIInsufficientArguments, msg = "match_id is a required argument for get_match_details"):
             d2api.APIWrapper().api_call(endpoints.GET_MATCH_DETAILS)
 
     def test_logger_check(self):
@@ -31,8 +34,7 @@ class APIPreliminaryTests(unittest.TestCase):
         'Logger should not be None when \'logging_enabled\' was set to True')
 
     def test_update_local_files(self):
-        tmp_api = d2api.APIWrapper()
-        self.assertNotEqual(tmp_api.update_local_data(), {},
+        self.assertNotEqual(update_local_data(), {},
         msg = "Update must return remote metadata. Instead, it returned an empty dict.")
 
 
@@ -47,33 +49,40 @@ class EndpointTests(unittest.TestCase):
         self.api_call = api.api_call
 
     def test_get_match_history_endpoint(self):
-        with self.assertRaises(APIAuthenticationError, msg = "GET_MATCH_HISTORY endpoint is not working as intended."):
+        with self.assertRaises(d2errors.APIAuthenticationError, msg = "GET_MATCH_HISTORY endpoint is not working as intended."):
             self.api_call(endpoints.GET_MATCH_HISTORY)
     
     def test_get_match_history_by_seq_num_endpoint(self):
-        with self.assertRaises(APIAuthenticationError, msg = "GET_MATCH_HISTORY_BY_SEQ_NUM endpoint is not working as intended."):
+        with self.assertRaises(d2errors.APIAuthenticationError, msg = "GET_MATCH_HISTORY_BY_SEQ_NUM endpoint is not working as intended."):
             self.api_call(endpoints.GET_MATCH_HISTORY_BY_SEQ_NUM)
     
     def test_get_match_details_endpoint(self):
-        with self.assertRaises(APIAuthenticationError, msg = "GET_MATCH_DETAILS endpoint is not working as intended."):
+        with self.assertRaises(d2errors.APIAuthenticationError, msg = "GET_MATCH_DETAILS endpoint is not working as intended."):
             self.api_call(endpoints.GET_MATCH_DETAILS, match_id = '123')
     
     def test_get_heroes_endpoint(self):
-        with self.assertRaises(APIAuthenticationError, msg = "GET_HEROES endpoint is not working as intended."):
+        with self.assertRaises(d2errors.APIAuthenticationError, msg = "GET_HEROES endpoint is not working as intended."):
             self.api_call(endpoints.GET_HEROES)
     
     def test_get_game_items_endpoint(self):
-        with self.assertRaises(APIAuthenticationError, msg = "GET_GAME_ITEMS endpoint is not working as intended."):
+        with self.assertRaises(d2errors.APIAuthenticationError, msg = "GET_GAME_ITEMS endpoint is not working as intended."):
             self.api_call(endpoints.GET_GAME_ITEMS)
 
-class EntityTests(unittest.TestCase):
+class DtypeTests(unittest.TestCase):
     def test_steam_32_64(self):
         steam32 = 123456
         steam64 = 76561197960265728 + steam32
-        account1 = SteamAccount(account_id = steam64)
-        account2 = SteamAccount(account_id = steam32)
+        account1 = entities.SteamAccount(account_id = steam64)
+        account2 = entities.SteamAccount(account_id = steam32)
         self.assertEqual(account1, account2,
         'SteamAccount created with 32 Bit or 64 Bit SteamID should be indistinguishable')
+    
+    def test_basewrapper(self):
+        test_obj = wrappers.BaseWrapper({'a':1, 'b':2, 'c':3})
+        test_obj2 = wrappers.BaseWrapper({'a':1, 'b':2, 'c':3})
+        self.assertEqual(test_obj.a, 1, 'BaseWrapper __getattr__ does not work as intended.')
+        self.assertEqual(test_obj['b'], 2, 'BaseWrapper __getitem__ does not work as intended.')
+        self.assertEqual(test_obj, test_obj2, 'BaseWrapper __eq__ does not work as intended.')
     
 
 
@@ -83,7 +92,7 @@ class MatchHistoryTests(unittest.TestCase):
         self.get_match_history = api.get_match_history
     
     def test_get_match_history_dtype(self):
-        self.assertIsInstance(self.get_match_history(), MatchHistory, 
+        self.assertIsInstance(self.get_match_history(), wrappers.MatchHistory, 
         'get_match_history() should return a MatchHistory object')
 
 class MatchDetailsTests(unittest.TestCase):
@@ -93,7 +102,7 @@ class MatchDetailsTests(unittest.TestCase):
     
     def test_get_match_details_dtype(self):
         match_id = '4176987886'
-        self.assertIsInstance(self.get_match_details(match_id), MatchDetails,
+        self.assertIsInstance(self.get_match_details(match_id), wrappers.MatchDetails,
         'get_match_details(\'{}\') should return a MatchDetails object'.format(match_id))
 
     def test_incorrect_matchid(self):
@@ -106,13 +115,13 @@ class HeroesTests(unittest.TestCase):
         self.get_heroes = api.get_heroes
     
     def test_get_heroes_dtype(self):
-        self.assertIsInstance(self.get_heroes(), Heroes,
+        self.assertIsInstance(self.get_heroes(), wrappers.Heroes,
         'get_heroes() should return a Heroes object')
 
     def test_hero_localized_name(self):
         res = self.get_heroes()
         cur_id = 59
-        cur_hero = [h for h in res.heroes() if h.id == cur_id][0]
+        cur_hero = [h for h in res.heroes if h.id == cur_id][0]
         self.assertEqual(cur_hero.localized_name, 'Huskar',
         'Localized name of id = {} should be Huskar'.format(cur_id))
 
@@ -122,16 +131,24 @@ class GameItemsTests(unittest.TestCase):
         self.get_game_items = api.get_game_items
     
     def test_get_game_items_dtype(self):
-        self.assertIsInstance(self.get_game_items(), GameItems,
+        self.assertIsInstance(self.get_game_items(), wrappers.GameItems,
         'get_game_items() should return a GameItems object')
+        
     def test_item_localized_name(self):
         res = self.get_game_items()
         cur_id = 265
-        cur_item = [i for i in res.items() if i.id == cur_id][0]
+        cur_item = [i for i in res.items if i.id == cur_id][0]
         self.assertEqual(cur_item.localized_name, 'Infused Raindrops',
         'Localized name of id = {} should be Infused Raindrops'.format(cur_id))
 
-
+class TournamentPrizePoolTests(unittest.TestCase):
+    def setUp(self):
+        api = d2api.APIWrapper()
+        self.get_tournament_prize_pool = api.get_tournament_prize_pool
+    
+    def test_get_tournament_prize_pool_dtype(self):
+        self.assertIsInstance(self.get_tournament_prize_pool(), wrappers.TournamentPrizePool,
+        'get_tournament_prize_pool() shoult return a TournamentPrizePool object.')
 
 # m = cur.get_match_details('4176987886')
 
