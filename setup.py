36e47284e3d8c2f5e9af2d94df84c864de420a9e
whoophee <raghavsairamn@gmail.com>
2019-07-25 00:41:24 -0500

    Added python 3.7 and removed 3.3 	modified:   .travis.yml 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -43,9 +43,9 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
     ]
 )
